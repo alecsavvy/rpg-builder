495a5940782cb26e7b3913735983938347fb1475
alecsavvy <alecsavvy@outlook.com>
2019-11-25 13:42:27 -0500

    initial commit

---
+++ src/main/scala/RpgApplication.scala
@@ -0,0 +1,3 @@
+abstract class RpgApplication extends App {
+  def RpgName : String
+}
---
+++ src/main/scala/RpgLoader.scala
@@ -0,0 +1,5 @@
+object RpgLoader extends RpgApplication {
+  override def RpgName = "alec's rpg"
+
+  println(s"Starting `$RpgName`!")
+}
